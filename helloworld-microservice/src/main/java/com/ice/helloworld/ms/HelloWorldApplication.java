f6b7082c9a03631a65fdd7234a25f173e7a8393c
bnagaraju2 <bnagaraju2@sapient.com>
2021-02-01 20:49:22 +0530

    Service Discovery Application

---
+++ helloworld-microservice/src/main/java/com/ice/helloworld/ms/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.ice.helloworld.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
